386755379f9f94940531a6744f561dfa06fc47c7
Jordan <jordanmittelman@gmail.com>
2019-07-19 10:38:14 -0400

    add user component

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,8 +1,55 @@
 import React, { Component } from "react";
+import "./User.css";
+import axios from "axios";
 
 class User extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    let url = "https://shift-change-api.herokuapp.com";
+    let extension = localStorage.userID;
+    axios
+      .get(url + "/api/users/id" + extension)
+      .then(res => this.setState({ user: res.data }))
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
-    return <div />;
+    return (
+      <div className="User">
+        {this.state.user.firstName && (
+          <div className="user-container">
+            {this.state.user.firstName && (
+              <div className="user-info-container">
+                <div>
+                  <h1>
+                    {this.state.user.firstName} {this.state.user.lastName}
+                  </h1>
+                </div>
+                <div>
+                  <h3>{this.state.user.email}</h3>
+                </div>
+              </div>
+            )}
+            <div className="user-agendas-container">
+              <div className="user-agendas-list">
+                <h3 className="agenda-dates">Your Agendas:</h3>
+                {this.state.user.myAgendas.map(user => {
+                  return <div className="user-myAgendas-list">{user.date}</div>;
+                })}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
